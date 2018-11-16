bd6d53cec0f378be769b33cef6e7da7694baf019
Oleg Mikhaylov <olegmixa@mail.ru>
2018-11-16 19:17:26 +0300

    itmo

---
+++ footer.php
@@ -0,0 +1,31 @@
+<div class="container contact">
+  <div class="form">
+    <div>
+      <div>
+        <label></label>
+        <input type="text" name="">
+      </div>
+      <div>
+        <label></label>
+        <input type="text" name="">
+      </div>
+    </div>
+    <div>
+      <label></label>
+      <textarea></textarea>
+    </div>
+    <div class="btn btn-bg">Send message</div>
+    <div class="btn">Clear</div>
+  </div>
+  <div class="info">
+    <div>
+      
+    </div>
+    <div>
+      
+    </div>
+    <div>
+      
+    </div>
+  </div>
+</div>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Курсовая</title>
+  <meta charset="utf-8">
+  <meta name="description" content="Курсовая работу по первому модулю NodeJS. ИТМО">
+  <meta name="author" content="OlegM">
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+  <header>
+    <?php include("head.php"); ?>
+  </header>
+  <main>
+    <section id="head">
+      <div class="banner-content">
+        <h1>Hi, my name is Forty</h1>
+        <hr>
+        <p>A RESPONSIVE SITE TEMPLATE DESIGNED BY HTML5 UP<br />AND RELEASED UNDER THE CREATIVE COMMONS.</p>
+      </div>
+    </section>
+    <section id="menu">
+      <article class="menu-block">
+        <header>
+          <h2>
+            <a href="#">Aliquam</a>
+          </h2>
+          <p></p>
+        </header>
+      </article>
+      <article class="menu-block">
+        <header>
+          <h2>
+            <a href="#"></a>
+          </h2>
+          <p></p>
+        </header>
+      </article>
+      <article class="menu-block">
+        <header>
+          <h2>
+            <a href="#"></a>
+          </h2>
+          <p></p>
+        </header>
+      </article>
+      <article class="menu-block">
+        <header>
+          <h2>
+            <a href="#"></a>
+          </h2>
+          <p></p>
+        </header>
+      </article>
+      <article class="menu-block">
+        <header>
+          <h2>
+            <a href="#"></a>
+          </h2>
+          <p></p>
+        </header>
+      </article>
+      <article class="menu-block">
+        <header>
+          <h2>
+            <a href="#"></a>
+          </h2>
+          <p></p>
+        </header>
+      </article>
+    </section>
+    <section id="chtoto">
+      <div class="container">
+        <div>
+          <h2>Massa libero</h2>
+          <p>
+            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.
+          </p>
+          <div class="btn">get started<span></span></div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <?php include("footer.php"); ?>
+  </footer>
+</body>
+</html>
