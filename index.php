1ff0f2b0eabb6f49f4fad594a82a40ae8bdf3031
Oleg Mikhaylov <olegmixa@mail.ru>
2018-11-18 23:02:00 +0300

    commit

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="description" content="Курсовая работу по первому модулю NodeJS. ИТМО">
   <meta name="author" content="OlegM">
   <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <link rel="stylesheet" href="/css/font-awesome.min.css">
 </head>
 <body>
   <header>
@@ -16,56 +17,59 @@
       <div class="banner-content">
         <h1>Hi, my name is Forty</h1>
         <hr>
-        <p>A RESPONSIVE SITE TEMPLATE DESIGNED BY HTML5 UP<br />AND RELEASED UNDER THE CREATIVE COMMONS.</p>
+        <div class="d-flex">
+          <p>A RESPONSIVE SITE TEMPLATE DESIGNED BY HTML5 UP<br />AND RELEASED UNDER THE CREATIVE COMMONS.</p>
+          <div class="btn d-inline banner-btn">get started&nbsp;<span class="fa fa-chevron-right"></span></div>
+        </div>
       </div>
     </section>
     <section id="menu">
       <article class="menu-block">
         <header>
           <h2>
-            <a href="#">Aliquam</a>
+            <a href="#">Button 1</a>
           </h2>
-          <p></p>
+          <p>Ipsum dolor sit amet</p>
         </header>
       </article>
       <article class="menu-block">
         <header>
           <h2>
-            <a href="#"></a>
+            <a href="#">Button 2</a>
           </h2>
-          <p></p>
+          <p>feugiat amet tempus</p>
         </header>
       </article>
       <article class="menu-block">
         <header>
           <h2>
-            <a href="#"></a>
+            <a href="#">Button 3</a>
           </h2>
-          <p></p>
+          <p>Lorem etiam nullam</p>
         </header>
       </article>
       <article class="menu-block">
         <header>
           <h2>
-            <a href="#"></a>
+            <a href="#">Button 4</a>
           </h2>
-          <p></p>
+          <p>Nisl sed aliquam</p>
         </header>
       </article>
       <article class="menu-block">
         <header>
           <h2>
-            <a href="#"></a>
+            <a href="#">Button 5</a>
           </h2>
-          <p></p>
+          <p>Ipsum dolor sit amet</p>
         </header>
       </article>
       <article class="menu-block">
         <header>
           <h2>
-            <a href="#"></a>
+            <a href="#">Button 6</a>
           </h2>
-          <p></p>
+          <p>Feugiat amet tempus</p>
         </header>
       </article>
     </section>
@@ -76,7 +80,7 @@
           <p>
             Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.
           </p>
-          <div class="btn">get started<span></span></div>
+          <div class="btn">get started&nbsp;<span class="fa fa-chevron-right"></span></div>
         </div>
       </div>
     </section>
