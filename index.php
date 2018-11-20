a098edabb39d680ee02b104ffc4015b67fba66ed
Олег Михайлов <olegmixa@mail.ru>
2018-11-20 13:33:55 +0300

    finish

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
   <meta charset="utf-8">
   <meta name="description" content="Курсовая работу по первому модулю NodeJS. ИТМО">
   <meta name="author" content="OlegM">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="/css/style.css">
   <link rel="stylesheet" href="/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script type="text/javascript" src="/scripts/script.js"></script>
 </head>
 <body>
   <header>
